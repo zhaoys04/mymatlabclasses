3cc6a58fffb3362aa8b16d024c01faaed67697aa
zhaoys04 <zhaoys04@gmail.com>
2015-05-22 10:34:15 -0400

    no revision, just change tools to use

--- @LIResult/AppendMeasurement.m
+++ @LIResult/AppendMeasurement.m
@@ -2,6 +2,9 @@ function f = AppendMeasurement(LI,I) % fixme: no exception handling
 	LI.hCurrentSource.Set_Current(I(1));
 	pause(1e-3);
 	LI.hPowerMeter.Get_Average_Power();
+    while LI.hPowerMeter.Power_Average > 100
+           LI.hPowerMeter.Get_Average_Power();
+    end
 	V = LI.hCurrentSource.Get_Voltage();
 	LI.Results.V = [LI.Results.V,V];
 	LI.Results.L = [LI.Results.L,LI.hPowerMeter.Power_Average];
