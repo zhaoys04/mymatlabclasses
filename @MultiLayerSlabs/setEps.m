e137cc586ffb6aaae5618e67acffd5d9ac55710e
Yunsong Zhao <zhaoys@Yunsong-PC.attlocal.net>
2017-05-25 22:53:35 -0700

    vim

--- @MultiLayerSlabs/setEps.m
+++ @MultiLayerSlabs/setEps.m
@@ -1,5 +1,5 @@
 function f = setEps(MLS,epsilon)
-	MLS.epsilon = [MLS.epsilon[1],epsilon,MLS.epsilon[end]];
+	MLS.epsilon = [MLS.epsilon(1),epsilon,MLS.epsilon(end)];
 	if (length(epsilon) ~= length(MLS.d))
 		display('WARNING: length of epsilon array is different from length of layer thickness array');
 	end
